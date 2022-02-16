c19f5af8f6bac37a0c11b54b54ff3cdc53aa5774
kamila.pereira <kamila.pereira@iteris.com.br>
2022-02-16 00:18:37 -0300

    criação do componente create contact list

---
+++ src/components/ContactList.vue
@@ -0,0 +1,75 @@
+<template>
+  <div class="info-contacts">
+    <p class="icon">{{ nome.charAt(0) }}</p>
+    <p>{{ nome }}</p>
+    <p>{{ email }}</p>
+    <p>{{ telefone }}</p>
+    <div class="buttons-contact">
+      <button class="btn-edit" @click="this.$emit('editContact')">
+        <img src="../assets/ic-edit.svg" alt="Editar" />
+      </button>
+      <button class="btn-delete">
+        <img
+          src="../assets/ic-delete.svg"
+          alt="Excluir"
+          @click="this.$emit('deleteContact')"
+        />
+      </button>
+    </div>
+  </div>
+  <slot />
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "ContactList",
+  emits: ["deleteContact", "editContact"],
+  props: {
+    id: {
+      type: Number,
+    },
+    nome: {
+      type: String,
+    },
+    email: {
+      type: String,
+    },
+    telefone: {
+      type: String,
+    },
+  },
+  methods: {},
+});
+</script>
+
+<style lang="scss" scoped>
+.info-contacts {
+  display: grid;
+  grid-template-columns: 0.4fr 4.7fr 4.7fr 4fr 0.7fr;
+  align-items: center;
+  padding: 12px 0;
+  font-size: 14px;
+  border-top: 1px solid #e1e1e1;
+
+  &:hover {
+    background-color: #fff3f2;
+  }
+
+  .icon {
+    font-size: 16px;
+    width: 24px;
+    height: 24px;
+    border-radius: 50%;
+    background-color: #fa8d68;
+    color: #ffff;
+    margin: 0 16px 0 8px;
+    padding: 3px 5px 2px 6px;
+  }
+
+  .btn-delete {
+    margin-left: 24px;
+  }
+}
+</style>
