ed3c3eb6e5355f621448d97f9b36aef679dc3fa9
kamila.pereira <kamila.pereira@iteris.com.br>
2022-02-16 00:19:11 -0300

    subida do app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,20 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <Content />
 </template>
 
 <script lang="ts">
+import Content from "./components/Content.vue";
 import { defineComponent } from "vue";
-import HelloWorld from "./components/HelloWorld.vue";
 
 export default defineComponent({
   name: "App",
   components: {
-    HelloWorld,
+    Content,
   },
 });
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+// #app {
+// }
 </style>
